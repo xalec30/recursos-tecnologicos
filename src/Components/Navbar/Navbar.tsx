a21ff6f6f9fdde42490bdcf31fbad5174e350c91
Alex Rodriguez <xalecr30@gmail.com>
2024-07-05 23:57:15 -0400

    nuevos cambios

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../Provider/AuthProvider";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSun,faMoon,faUser} from '@fortawesome/free-regular-svg-icons';
 import { faRightFromBracket,faTableColumns} from '@fortawesome/free-solid-svg-icons';
